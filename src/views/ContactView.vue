a80dbb97df8a05b901ec86956944c5ca7bda8097
Dcolor Wei <corfer@yeah.net>
2022-09-30 09:33:59 +0800

    feature: Views初始化

---
+++ src/views/ContactView.vue
@@ -0,0 +1,317 @@
+<template>
+  <div class="contact">
+    <div class="frame-left">
+      <div class="title">Contact us</div>
+      <div class="describe">Let AI boost your revenue......</div>
+      <div class="form">
+        <label>NAME</label>
+        <input required type="text" v-model="email.name" />
+        <label>EMAIL ADDRESS</label>
+        <input required type="email" v-model="email.email" />
+        <label>MESSAGE</label>
+        <textarea
+          required
+          v-model="email.content"
+          placeholder="Any message which you want to say..."
+        ></textarea>
+        <input
+          v-bind:style="{
+            visibility:
+              email.name && email.email && email.content ? 'visible' : 'hidden',
+          }"
+          class="submit"
+          value="SUBMIT"
+          type="submit"
+          @click="sendMail()"
+        />
+      </div>
+    </div>
+    <div class="frame-right">
+      <div class="title">Contact</div>
+      <div class="contact">
+        <div class="contact-item">
+          <div><u>Wechat</u></div>
+          <div><u>Mail</u></div>
+        </div>
+        <div class="contact-item">
+          <div>{{frontend.AdminInfo.email}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import endpoint from "@/doc/endpoint";
+import frontend from "@/doc/frontend";
+import { EmailReq } from "@/entity/request/SendEmailReq";
+import axios from "axios";
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  setup() {
+    const email = reactive({
+      name: "",
+      email: "",
+      content: "",
+    });
+    const sendMail = () => {
+      axios({
+        url: `${endpoint.SEND_EMAIL_TO_HOST}`,
+        method: "POST",
+        data: new EmailReq(`${email.name} ${email.email}`, email.content),
+      })
+        .then(() => {
+          email.name = "";
+          email.email = "";
+          email.content = "";
+          alert("Mail sent successfully!");
+        })
+        .catch(() => {
+          alert("Mail sent failed!");
+        });
+    };
+    return {
+      frontend,
+      email,
+      sendMail,
+    };
+  },
+});
+</script>
+
+<style>
+@media screen and (min-width: 650px) {
+  .contact {
+    width: 70%;
+    display: flex;
+    margin: 0 auto;
+  }
+  .contact .frame-left {
+    width: 80%;
+    margin: 0 50px;
+    padding: 40px;
+    border: 1px solid #eeeeee;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .contact .frame-left .title {
+    font-family: Montserrat;
+    font-size: 40px;
+    line-height: 100px;
+    font-weight: 600;
+    text-align: left;
+  }
+  .contact .frame-left .describe {
+    font-family: Montserrat;
+    letter-spacing: 1px;
+    font-size: 18px;
+    line-height: 50px;
+    font-weight: 300;
+    color: #7f7f7f;
+    text-align: left;
+  }
+
+  .contact .frame-left .form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .contact .frame-left .form label {
+    text-align: left;
+    font-family: Montserrat;
+    font-weight: 600;
+    font-size: 17px;
+    margin-top: 20px;
+  }
+  .contact .frame-left .form input,
+  .contact .frame-left .form textarea {
+    font-family: Microsoft YaHei;
+    font-size: 17px;
+    font-stretch: 100%;
+    font-weight: 400;
+    letter-spacing: 1px;
+    line-height: 26px;
+    margin: 25px 0;
+    padding: 0 3%;
+    outline-style: none;
+    transition-duration: 0.5s;
+  }
+
+  /* 隐藏侧边栏的scroll */
+  .contact .frame-left .form textarea::-webkit-scrollbar {
+    display: none;
+  }
+
+  .contact .frame-left .form input {
+    width: 32%;
+    height: 44px;
+  }
+  .contact .frame-left .form input:focus {
+    width: 92%;
+  }
+  .contact .frame-left .form textarea {
+    width: 92%;
+    height: 44px;
+    padding-top: 2.5%;
+  }
+  .contact .frame-left .form textarea:focus {
+    height: 144px;
+  }
+
+  .contact .frame-left .form .submit {
+    color: #fff;
+    margin: 20px 0;
+    border: none;
+    background-color: #1a1b1f;
+    width: 38%;
+  }
+
+  .contact .frame-right {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .contact .frame-right .title {
+    font-family: Montserrat;
+    font-size: 24px;
+    line-height: 60px;
+    font-weight: 600;
+    text-align: left;
+  }
+
+  .contact .frame-right .contact {
+    text-align: left;
+  }
+
+  .contact .frame-right .contact .contact-item {
+    margin-right: 20px;
+    font-size: 19px;
+    line-height: 40px;
+    font-family: Montserrat;
+    font-weight: 500;
+    letter-spacing: 1px;
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+@media screen and (max-width: 650px) {
+  .contact {
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+  .contact .frame-left {
+    width: 90%;
+    border: 1px solid #eeeeee;
+    padding: 0 5%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .contact .frame-left .title {
+    font-family: Montserrat;
+    font-size: 26px;
+    line-height: 50px;
+    font-weight: 700;
+    text-align: left;
+  }
+  .contact .frame-left .describe {
+    font-family: Montserrat;
+    letter-spacing: 1px;
+    font-size: 14px;
+    line-height: 40px;
+    font-weight: 300;
+    color: #7f7f7f;
+    text-align: left;
+  }
+
+  .contact .frame-left .form {
+    padding-left: 3%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .contact .frame-left .form label {
+    text-align: left;
+    font-family: Montserrat;
+    font-weight: 600;
+    font-size: 12px;
+    margin-top: 20px;
+  }
+  .contact .frame-left .form input,
+  .contact .frame-left .form textarea {
+    font-family: Microsoft YaHei;
+    font-size: 12px;
+    font-stretch: 100%;
+    letter-spacing: 1px;
+    padding: 0 2%;
+    width: 90%;
+    outline-style: none;
+    transition-duration: 0.5s;
+  }
+
+  .contact .frame-left .form input {
+    line-height: 26px;
+    font-weight: 400;
+    height: 30px;
+  }
+  .contact .frame-left .form textarea {
+    line-height: 20px;
+    height: 44px;
+    padding-top: 1%;
+  }
+  .contact .frame-left .form textarea:focus {
+    height: 144px;
+  }
+
+  .contact .frame-left .form .submit {
+    color: #fff;
+    margin: 20px 0;
+    border: none;
+    background-color: #1a1b1f;
+    width: 95%;
+  }
+
+  .contact .frame-right {
+    width: 100%;
+    margin: 3%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .contact .frame-right .title {
+    font-family: Montserrat;
+    font-size: 22px;
+    line-height: 60px;
+    font-weight: 500;
+    text-align: left;
+  }
+
+  .contact .frame-right .contact {
+    width: 100%;
+    text-align: left;
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+  }
+
+  .contact .frame-right .contact .contact-item {
+    width: 25%;
+    font-size: 14px;
+    line-height: 16px;
+    font-family: Montserrat;
+    font-weight: 400;
+    letter-spacing: 1px;
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,298 @@
+<template>
+  <div class="home">
+    <div class="show_unit banner-1">
+      <div class="title">Invest with AI</div>
+      <div class="subtitle">AI brings new insights in trading strategy</div>
+      <div class="learn-botton">LEARN MORE</div>
+    </div>
+    <div class="center-banner">
+      <div class="title">WE KNOW THE MARKET</div>
+      <div class="describe">
+        Understand the market in one more smart, swift, and accurate way.
+      </div>
+    </div>
+    <div class="show_unit banner-2">
+      <div class="left">
+        <div class="slogan">Data is Truth</div>
+        <div class="describe">
+          We obtain, store and understand market data from the world's largest
+          cryptocurrency exchange. Based on data without indulging in data. ‍
+        </div>
+        <div class="publicize">
+          More than 3000+ spot and futures contract symbols
+        </div>
+      </div>
+      <div class="right">
+        <div class="slogan">Code is Law</div>
+        <div class="describe">
+          Compared with irrational people, we always believe in algorithms. We
+          continue to improve the trading algorithms in order to obtain the
+          maximum return from the risk.
+        </div>
+        <div class="publicize">
+          CTA, Quantitative selection, Statistical Arbitrage and so on
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HomeView",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+@media screen and (min-width: 650px) {
+  .home {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .home .show_unit {
+    min-height: 600px;
+    width: 100%;
+  }
+  .home .banner-1 {
+    background: url("@/assets/icon/home/bc_price_moving.svg");
+    background-repeat: no-repeat;
+    background-position: 50% -30px;
+    background-size: 200% 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 200px;
+  }
+  .home .banner-1 .title {
+    font-family: Montserrat;
+    font-size: 70px;
+    font-weight: 400;
+    line-height: 90px;
+    letter-spacing: 4px;
+  }
+  .home .banner-1 .subtitle {
+    font-family: Montserrat;
+    font-size: 24px;
+    font-weight: 400;
+    color: #7a7a7a;
+    letter-spacing: 2px;
+    line-height: 34px;
+  }
+
+  .home .banner-1 .learn-botton {
+    margin-top: 45px;
+    padding: 15px 45px;
+    border-radius: 10px;
+    letter-spacing: 3px;
+    font-weight: 600;
+    transition: cubic-bezier(0.075, 0.82, 0.165, 1) 0.4s;
+  }
+
+  .home .banner-1 .learn-botton:hover {
+    background-color: gainsboro;
+  }
+
+  .center-banner {
+    width: 75%;
+    padding-bottom: 5%;
+    margin-bottom: 5%;
+    border-bottom: 1px solid #eeeeee;
+  }
+  .center-banner .title {
+    font-family: Montserrat;
+    font-weight: 600;
+    font-size: 14px;
+    color: #7a7a7a;
+    letter-spacing: 1px;
+  }
+  .center-banner .describe {
+    font-family: Montserrat;
+    margin-bottom: 15px;
+    outline-style: none;
+    outline-width: 3px;
+    color: #1a1b1f;
+    font-size: 40px;
+    margin: 3% auto;
+    line-height: 50px;
+    font-weight: 200;
+    letter-spacing: 2px;
+  }
+  .home .banner-2 {
+    background-size: 100%;
+    color: #fffffe;
+    display: flex;
+  }
+  .home .banner-2 .left {
+    background: linear-gradient(90deg, #9011a3 0%, #394caf);
+    width: 50%;
+    padding: 5%;
+    display: flex;
+    flex-direction: column;
+  }
+  .home .banner-2 .right {
+    background: linear-gradient(135deg, #e9465f 31%, #9c2fbf 100%);
+    width: 50%;
+    padding: 5%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .home .banner-2 .left div,
+  .home .banner-2 .right div {
+    height: 50%;
+    display: flex;
+    text-align: left;
+  }
+
+  .home .banner-2 .left .slogan,
+  .home .banner-2 .right .slogan {
+    font-size: 44px;
+    font-family: Montserrat;
+    font-weight: 700;
+  }
+
+  .home .banner-2 .left .describe,
+  .home .banner-2 .right .describe {
+    font-size: 20px;
+    font-family: Montserrat;
+    line-height: 32px;
+    font-weight: 600;
+  }
+
+  .home .banner-2 .left .publicize,
+  .home .banner-2 .right .publicize {
+    font-size: 44px;
+    font-family: Montserrat;
+    font-weight: 500;
+  }
+}
+
+@media screen and (max-width: 650px) {
+  .home {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .home .show_unit {
+    min-height: 400px;
+    width: 100%;
+  }
+  .home .banner-1 {
+    background: url("@/assets/icon/home/bc_price_moving.svg");
+    background-repeat: no-repeat;
+    background-position: 50% -100px;
+    background-size: 200% 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 100px;
+  }
+  .home .banner-1 .title {
+    font-family: Montserrat;
+    font-size: 70px;
+    font-weight: 400;
+    font-size: 36px;
+    letter-spacing: 4px;
+  }
+  .home .banner-1 .subtitle {
+    font-family: Montserrat;
+    font-size: 14px;
+    font-weight: 500;
+    opacity: 0.6;
+    letter-spacing: 1px;
+    line-height: 34px;
+  }
+
+  .home .banner-1 .learn-botton {
+    margin-top: 45px;
+    padding: 15px 45px;
+    border-radius: 10px;
+    letter-spacing: 3px;
+    font-weight: 600;
+    transition: cubic-bezier(0.075, 0.82, 0.165, 1) 0.4s;
+    background-color: gainsboro;
+  }
+
+  .center-banner {
+    width: 87%;
+    padding-bottom: 5%;
+    margin-bottom: 5%;
+    border-bottom: 1px solid #eeeeee;
+    
+  }
+  .center-banner .title {
+    font-size: 6px;
+    font-family: Montserrat;
+    font-weight: 600;
+    color: #7a7a7a;
+  }
+  .center-banner .describe {
+    font-family: Montserrat;
+    margin-bottom: 15px;
+    outline-style: none;
+    outline-width: 3px;
+    color: #1a1b1f;
+    font-size: 30px;
+    margin: 3% auto;
+    line-height: 50px;
+    font-weight: 300;
+    letter-spacing: 2px;
+  }
+
+  .home .banner-2 {
+    background-size: 100%;
+    color: #fffffe;
+    display: flex;
+    flex-direction: column;
+  }
+  .home .banner-2 .left {
+    background: linear-gradient(90deg, #9011a3 0%, #394caf);
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .home .banner-2 .right {
+    background: linear-gradient(135deg, #e9465f 31%, #9c2fbf 100%);
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .home .banner-2 .left div,
+  .home .banner-2 .right div {
+    height: 150px;
+    display: flex;
+    padding: 0 5%;
+    align-items: center;
+    text-align: left;
+  }
+
+  .home .banner-2 .left .slogan,
+  .home .banner-2 .right .slogan {
+    font-size: 32px;
+    font-family: Montserrat;
+    font-weight: 700;
+  }
+
+  .home .banner-2 .left .describe,
+  .home .banner-2 .right .describe {
+    font-size: 18px;
+    font-family: Montserrat;
+    font-weight: 600;
+  }
+
+  .home .banner-2 .left .publicize,
+  .home .banner-2 .right .publicize {
+    font-size: 28px;
+    font-family: Montserrat;
+    font-weight: 500;
+  }
+}
+</style>
